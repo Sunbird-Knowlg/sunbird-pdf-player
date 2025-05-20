b1b91f36db7df57841ff5bad45067e3b94e12ef4
yashashk <yashashk@sanketika.in>
2025-05-20 17:48:44 +0530

    Fix #FP-I43: Handle language resource bundle for PDF player

--- projects/sunbird-pdf-player/src/lib/sunbird-pdf-player.component.ts
+++ projects/sunbird-pdf-player/src/lib/sunbird-pdf-player.component.ts
@@ -20,7 +20,6 @@ import { ErrorService, errorCode, errorMessage } from '@project-sunbird/sunbird-
   styleUrls: ['./sunbird-pdf-player.component.scss']
 })
 export class SunbirdPdfPlayerComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit {
-  @Input() resourceService: any;
   public pdfConfig: Config;
   private subscription;
   public viewState = 'start';
